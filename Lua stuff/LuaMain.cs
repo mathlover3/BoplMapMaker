2b0097e56dd87c8720f70484286267dd12fa3322
David H <djhlovesmath@gmail.com>
2024-07-21 15:06:33 -0700

    bug fix

--- Lua stuff/LuaMain.cs
+++ Lua stuff/LuaMain.cs
@@ -569,18 +569,22 @@ namespace MapMaker.Lua_stuff
             if (index >= 1 && index <= 3)
             {
                 var slimeController = target.gameObject.GetComponent<SlimeController>();
+                if (slimeController.abilities.Count == 0)
+                {
+                    return 1000000;
+                }
                 if (slimeController.abilities.Count == 1)
                 {
-                    if (index == 1) return (double)(slimeController.abilityCooldownTimers[0] - slimeController.abilities[0].GetCooldown());
+                    if (index == 1) return (double)(slimeController.abilities[0].GetCooldown() - slimeController.abilityCooldownTimers[0]);
                     else return 1000000;
                 }
                 if (slimeController.abilities.Count == 2)
                 {
-                    if (index == 1) return (double)(slimeController.abilityCooldownTimers[0] - slimeController.abilities[0].GetCooldown());
-                    if (index == 2) return (double)(slimeController.abilityCooldownTimers[1] - slimeController.abilities[1].GetCooldown());
+                    if (index == 1) return (double)(slimeController.abilities[0].GetCooldown() - slimeController.abilityCooldownTimers[0]);
+                    if (index == 2) return (double)(slimeController.abilities[1].GetCooldown() - slimeController.abilityCooldownTimers[1]);
                     else return 1000000;
                 }
-                return (double)(slimeController.abilityCooldownTimers[index - 1] - slimeController.abilities[index - 1].GetCooldown());
+                return (double)(slimeController.abilities[index - 1].GetCooldown() - slimeController.abilityCooldownTimers[index - 1]);
             }
             else throw new ScriptRuntimeException($"index {index} is not a valid ability index. valid indexs are 1, 2 and 3");
 
@@ -590,6 +594,10 @@ namespace MapMaker.Lua_stuff
             if (index >= 1 && index <= 3)
             {
                 var slimeController = target.gameObject.GetComponent<SlimeController>();
+                if (slimeController.abilities.Count == 0)
+                {
+                    return;
+                }
                 if (slimeController.abilities.Count == 1)
                 {
                     if (index == 1)
@@ -615,6 +623,10 @@ namespace MapMaker.Lua_stuff
             if (index >= 1 && index <= 3)
             {
                 var slimeController = target.gameObject.GetComponent<SlimeController>();
+                if (slimeController.abilities.Count == 0)
+                {
+                    return 1000000;
+                }
                 if (slimeController.abilities.Count == 1)
                 {
                     if (index == 1) return (double)(slimeController.abilities[0].GetCooldown());
