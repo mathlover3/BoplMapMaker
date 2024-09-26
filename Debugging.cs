7c97d9b8418916b132c61226dd0c28d87fa33459
David H <djhlovesmath@gmail.com>
2024-09-26 13:46:57 -0700

    now it loops over amost all types (aside from generics and a few outers)

--- Debugging.cs
+++ Debugging.cs
@@ -3,9 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using BoplFixedMath;
 
 namespace MapMaker
 {
@@ -13,10 +17,42 @@ namespace MapMaker
     {
         public static void Awake()
         {
-            Type[] TypesToPatch = { typeof(Beam), typeof(BeamObject), typeof(BoplBody), typeof(DestroyIfOutsideSceneBounds), typeof(DetPhysics), typeof(DPhysicsBox), typeof(DPhysicsCircle), typeof(DPhysicsRoundedRect), typeof(DuplicatePlatformEffect), typeof(GameSessionHandler), typeof(InstantAbility), typeof(MeteorSmash), typeof(PlaceRocketEngine), typeof(PlaceSparkNode), typeof(PlatformTransform), typeof(PlayerBody), typeof(QuantumTunnel), typeof(ReviveParticle), typeof(Rope), typeof(RopeAttachment), typeof(Shake), typeof(SimpleSparkNode), typeof(TeleportedObjectEffect), typeof(TeleportIndicator), typeof(Updater), typeof(vibrate) };
+            Type[] allTypesInGameAssembly = typeof(Beam).Assembly.GetTypes();
+            List<Type> allTypes = new List<Type>();
+            allTypes.AddRange(allTypesInGameAssembly);
+            allTypes.Remove(typeof(FixTransform));
+            allTypesInGameAssembly = allTypes.ToArray();
+            allTypesInGameAssembly = Array.FindAll(allTypesInGameAssembly, IsAllowed);
+            allTypes = allTypesInGameAssembly.ToList();
+            UnityEngine.Debug.Log(allTypes.Count / 2);
+            var SecondHalf = allTypes.GetRange(allTypes.Count / 2-1, allTypes.Count/2);
+            var FirstForth = allTypes.GetRange(0, 45);
+            var ThirdEighth = allTypes.GetRange(45, 22);
+            var EighthSixteenth = allTypes.GetRange(80, 11);
+            SecondHalf.AddRange(FirstForth);
+            SecondHalf.AddRange(ThirdEighth);
+            SecondHalf.AddRange(EighthSixteenth);
+            SecondHalf.Add(typeof(Fix));
+            SecondHalf.Add(typeof(AABBXComparer));
+            SecondHalf.Add(typeof(AABBXComparer_roundedRectOnly));
+            allTypes.Remove(typeof(Circle));
+            allTypes.Remove(typeof(Box));
+            allTypes.Remove(typeof(RoundedRect));
+            SecondHalf.Add(typeof(Circle));
+            SecondHalf.Add(typeof(Box));
+            SecondHalf.Add(typeof(RoundedRect));
 
-            foreach (Type type in TypesToPatch)
+
+            LogDif(allTypes, SecondHalf);
+            Type[] TypesToPatch = { typeof(Beam), typeof(BeamObject), typeof(BlackHoleClap), typeof(BoplBody), typeof(BowTransform), typeof(CastSpell), typeof(ControlPlatform), typeof(Dash), typeof(DestroyIfOutsideSceneBounds), typeof(DetPhysics), typeof(DPhysicsBox), typeof(DPhysicsCircle), typeof(DPhysicsRoundedRect), typeof(Drill), typeof(DuplicatePlatformEffect), typeof(GameSessionHandler), typeof(Gravity), typeof(GunTransform), typeof(HookshotInstant), typeof(InstantAbility), typeof(MeteorSmash), typeof(PlaceRocketEngine), typeof(PlaceSparkNode), typeof(PlatformTransform), typeof(PlayerBody), typeof(QuantumTunnel), typeof(ReviveParticle), typeof(Rope), typeof(RopeAttachment), typeof(Shake), typeof(SimpleSparkNode), typeof(TeleportedObjectEffect), typeof(TeleportIndicator), typeof(Updater), typeof(vibrate) };
+
+            foreach (Type type in allTypes)
             {
+                //skip more internal stuff or if its a class in a class
+                if (type.FullName.Contains("<PrivateImplementationDetails>") || type.FullName.Contains("+"))
+                {
+                    continue;
+                }
                 Plugin.logger.LogInfo("Patching " + type.FullName);
                 MethodInfo[] methods = type.GetMethods();
 
@@ -28,6 +64,11 @@ namespace MapMaker
                     }
 
                     if (!method.HasMethodBody())
+                    {
+                        continue;
+                    }
+                    //if its a async meathed or a iterator skip it
+                    if (method.IsDefined(typeof(AsyncStateMachineAttribute), false) || method.IsDefined(typeof(IteratorStateMachineAttribute), false))
                     {
                         continue;
                     }
@@ -44,11 +85,32 @@ namespace MapMaker
             }
 
         }
+        public static bool IsAllowed(Type type)
+        {
+            //skip more internal stuff or if its a class in a class
+            return !type.IsGenericType && !type.IsDefined(typeof(CompilerGeneratedAttribute), false) && !type.FullName.Contains("<PrivateImplementationDetails>") && !type.FullName.Contains("+");
+        }
+        public static void LogDif(List<Type> list, List<Type> list2)
+        {
+            foreach (Type type in list)
+            {
+                if (!list2.Contains(type))
+                {
+                    UnityEngine.Debug.Log($"type is difrent: {type}");
+                }
+            }
+        }
+
     }
+
     public static class DebbugingTranspiler
     {
         static FieldInfo f_Pos_Field = AccessTools.Field(typeof(FixTransform), nameof(FixTransform.position));
-        static MethodInfo m_LoggingFunc = AccessTools.Method(typeof(DebbugingTranspiler), nameof(DebbugingTranspiler.Logging));
+        static FieldInfo f_ExternalVel_Field = AccessTools.Field(typeof(PlayerBody), nameof(PlayerBody.externalVelocity));
+        static FieldInfo f_selfImposedVel_Field = AccessTools.Field(typeof(PlayerBody), nameof(PlayerBody.selfImposedVelocity));
+        static MethodInfo m_LoggingPosFunc = AccessTools.Method(typeof(DebbugingTranspiler), nameof(DebbugingTranspiler.LoggingPos));
+        static MethodInfo m_LoggingExternalVelFunc = AccessTools.Method(typeof(DebbugingTranspiler), nameof(DebbugingTranspiler.LoggingExternalVel));
+        static MethodInfo m_LoggingselfImposedVelocityFunc = AccessTools.Method(typeof(DebbugingTranspiler), nameof(DebbugingTranspiler.LoggingselfImposedVelocity));
         public static IEnumerable<CodeInstruction> FieldTranspiler(IEnumerable<CodeInstruction> instructions)
         {
             var codes = new List<CodeInstruction>(instructions);
@@ -59,18 +121,48 @@ namespace MapMaker
                     //if it stores something to the fixtrasforms posison then insert our code
                     yield return new CodeInstruction(System.Reflection.Emit.OpCodes.Dup);
                     yield return new CodeInstruction(System.Reflection.Emit.OpCodes.Ldarg_0);
-                    yield return new CodeInstruction(System.Reflection.Emit.OpCodes.Call, m_LoggingFunc);
+                    yield return new CodeInstruction(System.Reflection.Emit.OpCodes.Call, m_LoggingPosFunc);
+                }
+                if (instruction.StoresField(f_ExternalVel_Field))
+                {
+                    //if it stores something to the fixtrasforms posison then insert our code
+                    yield return new CodeInstruction(System.Reflection.Emit.OpCodes.Dup);
+                    yield return new CodeInstruction(System.Reflection.Emit.OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(System.Reflection.Emit.OpCodes.Call, m_LoggingExternalVelFunc);
+                }
+                if (instruction.StoresField(f_selfImposedVel_Field))
+                {
+                    //if it stores something to the fixtrasforms posison then insert our code
+                    yield return new CodeInstruction(System.Reflection.Emit.OpCodes.Dup);
+                    yield return new CodeInstruction(System.Reflection.Emit.OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(System.Reflection.Emit.OpCodes.Call, m_LoggingselfImposedVelocityFunc);
                 }
                 yield return instruction;
             }
 
         }
-        public static void Logging(Vec2 newPos, UnityEngine.Component component)
+        public static void LoggingPos(Vec2 newPos, UnityEngine.Component component)
         {
             var gameobj = component.gameObject;
             if (gameobj.name == "Player(Clone)")
             {
                 UnityEngine.Debug.Log($"{UnityEngine.StackTraceUtility.ExtractStackTrace()} is changing player pos, new value: {newPos}, at time: {Updater.SimTimeSinceLevelLoaded}");
+            }
+        }
+        public static void LoggingExternalVel(Vec2 newExternalVel, UnityEngine.Component component)
+        {
+            var gameobj = component.gameObject;
+            if (gameobj.name == "Player(Clone)")
+            {
+                UnityEngine.Debug.Log($"{UnityEngine.StackTraceUtility.ExtractStackTrace()} is changing player externalVelocity, new value: {newExternalVel}, at time: {Updater.SimTimeSinceLevelLoaded}");
+            }
+        }
+        public static void LoggingselfImposedVelocity(Vec2 newselfImposedVelocity, UnityEngine.Component component)
+        {
+            var gameobj = component.gameObject;
+            if (gameobj.name == "Player(Clone)")
+            {
+                UnityEngine.Debug.Log($"{UnityEngine.StackTraceUtility.ExtractStackTrace()} is changing player selfImposedVelocity, new value: {newselfImposedVelocity}, at time: {Updater.SimTimeSinceLevelLoaded}");
             }
         }
     }
--- obj/Debug/net472/MapMaker.AssemblyInfo.cs
+++ obj/Debug/net472/MapMaker.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("My first plugin")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa5d798e52c79e27f937358bd4851db6ed489974")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bfda4c22020c61ded5b2b4ccb457b5d2ae2e44a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MapMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MapMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
