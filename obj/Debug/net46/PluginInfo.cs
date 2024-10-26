2cb273d1ce0901828945217f3ee90f66b0ea0cc5
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-10-26 14:45:57 -0600

    Delete obj directory

--- obj/Debug/net46/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BoplMapMaker
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "BoplMapMaker";
-        public const string PLUGIN_NAME = "BoplMapMaker";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
--- obj/Debug/net48/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace AntiKick
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "AntiKick";
-        public const string PLUGIN_NAME = "AntiKick";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
